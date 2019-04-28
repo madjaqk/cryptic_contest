e9cfe1bab921fcd223a5b4c006d45e8799207209
Jack Brounstein <jack.brounstein@yahoo.com>
2019-04-27 20:13:21 -0500

    fix manage.py command to pass update_contest_status task just the contest ID

--- apps/cryptics/management/commands/create_celery_tasks.py
+++ apps/cryptics/management/commands/create_celery_tasks.py
@@ -14,14 +14,14 @@ class Command(BaseCommand):
         for contest in Contest.objects.filter(created_at__gt=earliest_time):
             if contest.voting_end_time > timezone.now():
                 update_contest_status.apply_async(
-                    args=(contest,),
+                    args=(contest.id,),
                     eta=contest.voting_end_time+timedelta(seconds=1)
                 )
                 self.stdout.write(f"Created end voting task for {contest.word}")
                 # This is intentionally one level deeper
                 if contest.submissions_end_time > timezone.now():
                     update_contest_status.apply_async(
-                        args=(contest,),
+                        args=(contest.id,),
                         eta=contest.submissions_end_time+timedelta(seconds=1)
                     )
                     self.stdout.write(f"Created end submission task for {contest.word}")
